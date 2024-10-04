a78417a4de51944640a2e22ff629054720e006d8
Chidike Henry <30509335+lacegiovanni17@users.noreply.github.com>
2024-10-04 23:57:12 +0100

    feat:added more styling to sidebar

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -4,7 +4,12 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div>
+      <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+        <div>Logo</div>
+        <h1 className='font-extrabold text-2xl'>GSTOCK</h1>
+      </div>
+    </div>
   )
 }
 
