6e800030e9f0eb08bb3a6c7acf4c3d75437e09b6
Chidike Henry <30509335+lacegiovanni17@users.noreply.github.com>
2024-10-10 23:57:33 +0100

    feat: added dark mode for css in dashboard layout

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -7,8 +7,9 @@ import StoreProvider, { useAppSelector} from './redux';
 
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
   return (
-    <div className={`light flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
+    <div className={`${isDarkMode ? "dark" : "light"} flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
       Sidebar
       <Sidebar />
       <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}`}>
