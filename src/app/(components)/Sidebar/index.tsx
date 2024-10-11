67b04815e41151c2e5aa35f1955d62585e0036a9
Chidike Henry <30509335+lacegiovanni17@users.noreply.github.com>
2024-10-11 18:40:08 +0100

    feat: onlick edits

--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -1,18 +1,54 @@
 "use client"
-import { Menu } from 'lucide-react'
+import { useAppDispatch, useAppSelector } from '@/app/redux'
+import { setIsSidebarCollapsed } from '@/state'
+import { LucideIcon, Menu } from 'lucide-react'
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
 import React from 'react'
 
+interface SidebarLinkProps {
+  href: string,
+  icon: LucideIcon,
+  label: string,
+  isCollapsed: boolean,
+}
 
+const SidebarLink = ({
+  href,
+  icon: Icon,
+  label,
+  isCollapsed,
+}: SidebarLinkProps) => {
+  const pathname = usePathname();
+  const isActive = pathname === href || (pathname === "/" && href === "/dashboard")
+  return (
+    <Link href={href} >
+      <div className={`cursor-pointer flex items-center ${isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"} hover:text-blue-500 `}>
+
+      </div>
+    </Link>
+  )
+}
 
 const Sidebar = () => {
+  const dispatch = useAppDispatch();
+  const isSidebarCollapsed = useAppSelector(
+    (state) => state.global.isSidebarCollapsed
+  );
+  const toggleSidebar = () => {
+    dispatch(setIsSidebarCollapsed(!setIsSidebarCollapsed));
+  };
+
+  const sidebarClassNames = `fixed flex flex-col ${
+    isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+  } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`
   return (
-    <div>
+    <div className={sidebarClassNames}>
       {/* TOP LOGO */}
-      <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+      <div className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${isSidebarCollapsed ? "px-5" : "px-8"}`}>
         <div>Logo</div>
-        <h1 className='font-extrabold text-2xl'>GSTOCK</h1>
-      
-      <button className='md:hidden px-3 py-3  bg-gray-100 rounded-full hover:bg-blue-100' onClick={() => { }} title='button'>
+        <h1 className={`${isSidebarCollapsed ? "hidden" : "block"} font-extrabold text-2xl`}>GSTOCK</h1>
+      <button className='md:hidden px-3 py-3  bg-gray-100 rounded-full hover:bg-blue-100' onClick={toggleSidebar} title='button'>
           <Menu className='w-4 h-4' />
       </button>
       </div>
