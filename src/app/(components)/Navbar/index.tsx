9417470eee9071edf9198116d624f0b6861e9e5b
Chidike Henry <30509335+lacegiovanni17@users.noreply.github.com>
2024-10-13 23:55:42 +0100

    feat: added more

--- src/app/(components)/Navbar/index.tsx
+++ src/app/(components)/Navbar/index.tsx
@@ -68,13 +68,14 @@ const Navbar = () => {
           <hr className="w-0 h-7 border border-solid border-l border-gray-300 mx-3" />
           <div className="flex items-center gap-3 cursor-pointer">
             <Image
+            //   src="https://google.com"
               src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/profile.jpg"
               alt="Profile"
               width={50}
               height={50}
               className="rounded-full h-full object-cover"
             />
-            <span className="font-semibold">Ed Roh</span>
+            <span className="font-semibold">G Stock</span>
           </div>
         </div>
         <Link href="/settings">
--- src/app/(components)/Sidebar/index.tsx
+++ src/app/(components)/Sidebar/index.tsx
@@ -83,7 +83,7 @@ const Sidebar = () => {
       >
         <Image
           src="https://s3-inventorymanagement.s3.us-east-2.amazonaws.com/logo.png"
-          alt="edstock-logo"
+          alt="gstock-logo"
           width={27}
           height={27}
           className="rounded w-8"
@@ -93,7 +93,7 @@ const Sidebar = () => {
             isSidebarCollapsed ? "hidden" : "block"
           } font-extrabold text-2xl`}
         >
-          EDSTOCK
+          GSTOCK
         </h1>
 
         <button
@@ -147,7 +147,7 @@ const Sidebar = () => {
 
       {/* FOOTER */}
       <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
-        <p className="text-center text-xs text-gray-500">&copy; 2024 Edstock</p>
+        <p className="text-center text-xs text-gray-500">&copy; 2024 Gstock</p>
       </div>
     </div>
   );
