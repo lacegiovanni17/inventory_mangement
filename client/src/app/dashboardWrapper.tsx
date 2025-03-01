f50280278be63725d1b2fa39c5b061acedf9d30d
Chidike Henry <chidike.henry@gmail.com>
2025-03-01 12:37:50 +0100

    feat: created new folder for client frontend and moved previous files in

---
+++ client/src/app/dashboardWrapper.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react'
+
+
+const DashboardWrapper = ({ children}: {children: React.ReactNode}) => {
+  return (
+    <div>DashoardWrapper {children}</div>
+  )
+}
+
+export default DashboardWrapper
