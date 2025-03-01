c37a6b1f5179b63f27b9ea28480e9eb3e3d6e5cc
Chidike Henry <chidike.henry@gmail.com>
2025-03-01 12:10:02 +0100

    fix: rewriting codes to make client UI active

--- src/app/dashboardWrapper.tsx
+++ src/app/dashboardWrapper.tsx
@@ -1,41 +1,11 @@
-"use client";
-import React, { useEffect } from 'react'
-import Navbar from "@/app/(components)/Navbar";
-import Sidebar from '@/app/(components)/Sidebar';
-import StoreProvider, { useAppSelector} from './redux';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React from 'react'
 
 
-
-const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  const isSidebarCollapsed = useAppSelector((state) => state.global.isSidebarCollapsed);
-  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
-
-  useEffect(() => {
-    if (isDarkMode) {
-      document.documentElement.classList.add("dark")
-    } else {
-      document.documentElement.classList.add("light")
-    }
-  })
-
+const DashboardWrapper = ({ children}: {children: React.ReactNode}) => {
   return (
-    <div className={`${isDarkMode ? "dark" : "light"} flex bg-gray-50 text-gray-900 w-full min-h-screen`}>
-      Sidebar
-      <Sidebar />
-      <main className={`flex flex-col w-full h-full py-7 px-9 bg-gray-50 ${isSidebarCollapsed ? "md:pl-24" : "md:pl-72"}`}>
-        <Navbar />{children}
-      </main>
-    </div>
+    <div>DashoardWrapper {children}</div>
   )
 }
 
-
-const DashboardWrapper = ({children}: {children:React.ReactNode}) => {
-  return (
-    <StoreProvider>
-      <DashboardLayout>{children}</DashboardLayout>
-    </StoreProvider>
-  )
-}
-
-export default DashboardWrapper;
+export default DashboardWrapper
