3b199c8808b15bdcc710408774eee77adc0937ee
Chidike Henry <chidike.henry@gmail.com>
2025-03-18 23:58:55 +0100

    feat: added card purchase summary to the dashboard

---
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useGetDashboardMetricsQuery } from '@/state/api';
+import React from 'react'
+
+const CardPurchaseSummary = () => {
+    const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+  return (
+    <div className='row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500'>
+        CardPurchaseSummary
+    </div>
+  )
+}
+
+export default CardPurchaseSummary
