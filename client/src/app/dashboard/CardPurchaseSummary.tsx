8ed24fd90a21cb41200726c711727e6fb7737673
Chidike Henry <chidike.henry@gmail.com>
2025-03-19 23:59:15 +0100

    feat: added styling to css

--- client/src/app/dashboard/CardPurchaseSummary.tsx
+++ client/src/app/dashboard/CardPurchaseSummary.tsx
@@ -4,9 +4,24 @@ import React from 'react'
 
 const CardPurchaseSummary = () => {
     const { data: dashboardMetrics, isLoading } = useGetDashboardMetricsQuery();
+    // const purchaseData = data?.purchaseSummary || [];
+
   return (
-    <div className='row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500'>
-        CardPurchaseSummary
+    <div className="flex flex-col justify-between row-span-2 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-white shadow-md rounded-2xl">
+       {isLoading ? (
+        <div className="m-5">Loading...</div>
+      ) : (
+        <>
+          {/* HEADER */}
+          <div>
+            <h2 className="text-lg font-semibold mb-2 px-7 pt-5">
+              Purchase Summary
+            </h2>
+            <hr />
+          </div>
+          {/* BODY */}
+          </>
+      )}
     </div>
   )
 }
